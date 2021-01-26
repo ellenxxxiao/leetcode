cc5b023ac90563c2c8f2fe5f607490e866d71c83
ellen xiao <ellenxiao0412@gmail.com>
2021-01-26 18:24:44 +0800

    Problem 1&2

---
+++ src/leetcode/editor/en/P1TwoSum.java
@@ -0,0 +1,135 @@
+//Given an array of integers nums and an integer target, return indices of the t
+//wo numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 103 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// Only one valid answer exists. 
+// 
+// Related Topics Array Hash Table 
+// 👍 18936 👎 675
+
+
+package leetcode.editor.en;
+
+import java.util.HashMap;
+
+//Java：Two Sum
+public class P1TwoSum{
+    public static void main(String[] args) {
+        Solution solution = new P1TwoSum().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+/*
+    Brute-force approach.
+    @Time complexity: O(N^2)
+    @Space complexity: O(1)
+
+    每一个数字和自己后面的所有数字依次凑凑试试，能否等于target。不和前面的比是因为已比过。直接可得双for loop嵌套。
+ */
+class Solution_1 {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=0; i<nums.length; i++){
+            for (int j=i+1; j<nums.length; j++){
+                if (nums[i]+nums[j] == target){
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // 全部loop完还是没有找到，raise exception
+        throw new IllegalArgumentException("No two sum solution.");
+    }
+}
+
+
+/*
+    Two-pass Hash Table approach.
+    @Time complexity: O(N)
+    @Space complexity: O(N)
+
+    使用一个hash map。
+    first pass：读一次array内容，把每个值和对应index写入hash map中。
+    second pass：再读一次array内容，找每个值complement是否存在于表中，如果存在，直接读出index。
+ */
+class Solution_2 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // first pass
+        for(int i=0; i<nums.length; i++){
+            // put in (value, index) pairs.
+            map.put(nums[i], i);
+        }
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        // 全部loop完还是没有找到，raise exception
+        throw new IllegalArgumentException("No two sum solution.");
+    }
+}
+
+
+/*
+    One-pass Hash Table approach.
+    @Time complexity: O(N) - 但是比上一种
+    @Space complexity: O(N)
+
+    使用一个hash map。
+    Only pass：在写入的同时找complement。
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // Only pass
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            // 不用check complement是不是自己，因为在当前时刻"自己"还没有放进去。
+            if (map.containsKey(complement)){
+                return new int[] {i, map.get(complement)};
+            }
+            // 如果没有找到complement，则put in (value, index) pairs，预备下一轮。
+            map.put(nums[i], i);
+        }
+        // 全部loop完还是没有找到，raise exception
+        throw new IllegalArgumentException("No two sum solution.");
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/en/P2AddTwoNumbers.java
@@ -0,0 +1,87 @@
+//You are given two non-empty linked lists representing two non-negative integer
+//s. The digits are stored in reverse order, and each of their nodes contains a si
+//ngle digit. Add the two numbers and return the sum as a linked list. 
+//
+// You may assume the two numbers do not contain any leading zero, except the nu
+//mber 0 itself. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+//Explanation: 9999999 + 9999 = 10009998.
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in each linked list is in the range [1, 100]. 
+// 0 <= Node.val <= 9 
+// It is guaranteed that the list represents a number that does not have leading
+// zeros. 
+// 
+// Related Topics Linked List Math Recursion 
+// 👍 10588 👎 2589
+
+
+package leetcode.editor.en;
+
+//Java：Add Two Numbers
+public class P2AddTwoNumbers{
+    public static void main(String[] args) {
+        Solution solution = new P2AddTwoNumbers().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/*
+    @Time complexity:
+    @Space complexity:
+
+    思路：
+    因为是倒序给的linked list（从least sig位到most sig位），所以直接依次相加。
+    有可能会有overflow造成的carry bit（进位），这个carry bit是1或0。
+    故每位上的相加操作需要公式：leftNode.val + rightNode.val + carry bit。（最大值即：9+9+1 = 19）
+    为方便开始追溯，需要一个dummy head，方便我们找到起点。dummyHead.next即为答案ListNode。
+
+    注意：
+    三种情况。
+    1. 两个list不一样长。（一个会先走完）
+    2. list为null。
+    3. [9,9] 和 [1]。两位长和一位长，相加得三位长。
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
